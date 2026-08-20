d3c0484c4e78f25f322e341ab6169a1d44413db8
Vik <vikramchahal21@gmail.com>
2026-08-19 17:29:15 -0700

    Use 512 icon for PawChart favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,11 +27,8 @@ export const metadata: Metadata = {
     title: brand.shortName,
   },
   icons: {
-    icon: [
-      { url: "/icons/icon-192.png", sizes: "192x192", type: "image/png" },
-      { url: "/icons/icon-512.png", sizes: "512x512", type: "image/png" },
-    ],
-    apple: [{ url: "/icons/icon-192.png", sizes: "192x192", type: "image/png" }],
+    icon: [{ url: "/icons/icon-512.png", sizes: "512x512", type: "image/png" }],
+    apple: [{ url: "/icons/icon-512.png", sizes: "512x512", type: "image/png" }],
   },
 };
 
